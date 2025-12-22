59c15e2c1ae270fad72664d03382944c7fd3a0c9
ruaji <a398286230@gmail.com>
2025-12-22 16:38:42 +0800

    去掉本地harmony依赖

--- Source/obj/Debug/RimTalkHealthEnhance.AssemblyInfo.cs
+++ Source/obj/Debug/RimTalkHealthEnhance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RimTalkHealthEnhance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b597c9601c3027557dfc90ea6001da1472e27194")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8c0ff7c9ca5cb676844e974004daa374b68df3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RimTalkHealthEnhance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RimTalkHealthEnhance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
